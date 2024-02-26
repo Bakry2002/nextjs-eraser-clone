783a849813404bb8b3bacc3308dc357fc475a244
Abdullah M. Bakry <abdaullah62@gmail.com>
2024-02-26 23:04:17 +0200

    Update page component and add new dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
-import Image from "next/image";
+import Header from '@/components/shared/header';
+import Image from 'next/image';
 
 export default function Home() {
-  return (
-    <div>
-      <h1>
-        <Image src="/vercel.svg" alt="Vercel Logo" width="100" height="100" />
-        Welcome to Next.js!
-      </h1>
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+        </div>
+    );
 }
---
+++ components/sections/hero.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const HerSection = () => {
+    return <div>HerSection</div>;
+};
+
+export default HerSection;
